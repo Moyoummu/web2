498a6df8e81ea262a52847a3c5faf4106f2c79c9
Moyoummu <maxbalaklf@gmail.com>
2023-10-10 11:42:41 +0300

    hw3 added request counter

--- hw33.py
+++ hw33.py
@@ -3,14 +3,23 @@ import tornado.ioloop
 import tornado.httpserver
 import os
 
+req_counter = 0
+
 class htmlHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('h2.html')
+
+class RequestCountHandler(tornado.web.RequestHandler):
+    def get(self):
+        global req_counter
+        req_counter += 1
+        self.write(f'Номер запроса:  {req_counter}')
 
 class webApp(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', htmlHandler),
+            (r'/#request', RequestCountHandler),
             (r'/(.*)', tornado.web.StaticFileHandler, {'path': os.path.abspath(__file__)})
 
         ]
